d37fecf849375391cb7d059dd5326334d1bd2d36
Igor <igorekimow@gmail.com>
2020-12-12 19:06:56 +0300

    Import/Export

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,7 @@
-console.log('app worked')
+console.log('app worked');
+
+import Game from './game';
+import {GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame} from './game';
+
+const game = new Game();
+game.start();
